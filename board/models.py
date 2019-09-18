860573cff7cc7020f38e83b64194416af397d553
gitaudo <s213407@studenti.polito.it>
2019-09-18 15:05:02 +0200

    Added initial venture model

--- board/models.py
+++ board/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Venture(models.Model):
+    PHASES = [
+        ('I', 'Ideazione'),
+        ('S', 'Sviluppo'), 
+        ('D', 'Distribuzione'),        
+    ]
+    
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publication_date = models.DateField()
+    tag = models.CharField(max_length=100)
+    phase = models.CharField(
+        max_length=1,
+        choices=PHASES,
+    )
+    
+    def __str__(self):
+        return self.title
