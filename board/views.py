3c5ffbf5e404ff68ab9487148fc7992a46941106
gitaudo <s213407@studenti.polito.it>
2019-09-18 22:52:50 +0200

    Added homepage template rendering

--- board/views.py
+++ board/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from board.models import Venture
 
+class VentureListView(generic.ListView):
+    model = Venture
+    template_name = 'index.html'
+    
 def index(request):
-    return render(request, 'index.html')
+    context['ventures']
+    for venture in Venture.objects.all():
+        context += venture
+
+        
+    return render(request, 'index.html', context=context)
 
